2f570361c0cd1bb7ca414ca91e6d55d861199574
Koenvh <koen@koenvh.nl>
2015-06-25 17:54:56 +0200

    Reverted accidental name change

--- USB Ejected Check/frmAlert.Designer.vb
+++ USB Ejected Check/frmAlert.Designer.vb
@@ -70,7 +70,7 @@ Partial Class frmAlert
         Me.Name = "frmAlert"
         Me.Opacity = 0.6R
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "h"
+        Me.Text = "Please eject your USB drive"
         Me.TopMost = True
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
         Me.ResumeLayout(False)
